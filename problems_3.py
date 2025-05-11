3ec8bb7615d14f536dcaac8a60a580f725806216
Usman Bajwa <ua65938@gmail.com>
2025-05-11 22:40:23 +0500

    felipe_taqueria.py

--- problems_3.py
+++ problems_3.py
@@ -56,6 +56,31 @@ input case insensitively. Ignore any input that isn’t an item. Assume that eve
 '''
 #############################################################################################################################
 
+def felipe_taqueria():
+    Menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+    total_bill = 0
+    while True:
+        try:
+            order = input("Item: ")
+            total_bill += Menu[order.title()] 
+            print(f"${total_bill}")
+        except KeyError:
+            pass
+        except EOFError:
+            break
+
+            
+
 
 
 #############################################################################################################################
