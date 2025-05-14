1406220ec25de2ddbc5c019be8411c93159d0c80
Usman Bajwa <ua65938@gmail.com>
2025-05-14 23:03:35 +0500

    problems_4.py

--- problems_4.py
+++ problems_4.py
@@ -76,3 +76,47 @@ If the guess is the same as that integer, the program should output Just right! 
 '''
 #############################################################################################################################
 
+
+
+#############################################################################################################################
+'''
+One of David’s first toys as a child, funny enough, was Little Professor, a “calculator” that would generate ten different math
+problems for David to solve. For instance, if the toy were to display 4 + 0 = , David would (hopefully) answer with 4. If the 
+toy were to display 4 + 1 = , David would (hopefully) answer with 5. If David were to answer incorrectly, the toy would 
+display EEE. And after three incorrect answers for the same problem, the toy would simply display the correct answer (e.g., 4 + 0 = 4 or 4 + 1 = 5).
+In a file called professor.py, implement a program that:
+Prompts the user for a level, 
+.If the user does not input 1, 2, or 3, the program should prompt again.
+Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with 
+digits. No need to support operations other than addition (+).
+Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should 
+output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has still not
+answered correctly after three tries, the program should output the correct answer.
+The program should ultimately output the user’s score: the number of correct answers out of 10.
+Structure your program as follows, wherein get_level prompts (and, if need be, re-prompts) the user for a level and 
+returns 1, 2, or 3, and generate_integer returns a randomly generated non-negative integer with level digits or raises a 
+ValueError if level is not 1, 2, or 3:
+'''
+#############################################################################################################################
+
+
+
+
+#############################################################################################################################
+'''
+I’m thinking of a number between 1 and 100…
+
+What is it?
+In a file called game.py, implement a program that:
+
+Prompts the user for a level, 
+. If the user does not input a positive integer, the program should prompt again.
+Randomly generates an integer between 1 and 
+, inclusive, using the random module.
+Prompts the user to guess that integer. If the guess is not a positive integer, the program should prompt the user again.
+If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
+If the guess is larger than that integer, the program should output Too large! and prompt the user again.
+If the guess is the same as that integer, the program should output Just right! and exit.
+'''
+#############################################################################################################################
+
