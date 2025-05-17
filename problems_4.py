5f8e4b00977155b7644adc1fb78e671205a0f45f
Usman Bajwa <ua65938@gmail.com>
2025-05-17 23:44:03 +0500

    litterprofessor.py

--- problems_4.py
+++ problems_4.py
@@ -179,7 +179,18 @@ def get_level():
 
 
 def generate_integer(level):
-    pass
+    for i in range(10):
+        if level == 1:
+            x = random.randint(1,10)
+            y = random.randint(1,10)
+        elif level == 2:
+            x = random.randint(10,100)
+            y = random.randint(10,100)
+        else:
+            x = random.randint(100,1000)
+            y = random.randint(100,1000)
+        ans = int(input(f"{x} + {y} = "))
+        
 
 
 if __name__ == "__main__":
