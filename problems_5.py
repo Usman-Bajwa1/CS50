3f54084ad5283f4647ea71b0dedb79263c47805a
Usman Bajwa <ua65938@gmail.com>
2025-05-20 22:30:09 +0500

    problems_5.py

--- problems_5.py
+++ problems_5.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
 
 Then, in a file called test_twttr.py, implement one or more functions that collectively test your implementation of
 shorten thoroughly, each of whose names should begin with test_ so that you can execute your tests with:
+pytest test_twttr.py
 '''
 #############################################################################################################################
 
