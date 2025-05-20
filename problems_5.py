daf51e74020d6277c1b291f6f779be05a65af958
Usman Bajwa <ua65938@gmail.com>
2025-05-20 22:32:13 +0500

    problems_5.py

--- problems_5.py
+++ problems_5.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
     main()
 Then, in a file called test_plates.py, implement four or more functions that collectively test your implementation of 
 is_valid thoroughly, each of whose names should begin with test_ so that you can execute your tests with:
+pytest test_plates.py
 '''
 #############################################################################################################################
 
