2f11171b129e4ea4621104f87075249f4c4ba636
Usman Bajwa <ua65938@gmail.com>
2025-05-31 23:29:17 +0500

    problems_7.py

--- problems_7.py
+++ problems_7.py
@@ -165,3 +165,23 @@ that collectively test your implementation of count thoroughly, each of whose na
 '''
 #############################################################################################################################
 
+
+
+#############################################################################################################################
+'''
+When creating a Google Form that prompts users for a short answer (or paragraph), it’s possible to enable response validation
+and require that the user’s input match a regular expression. For instance, you could require that a user input an email address
+with a regex like this one:
+
+^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$
+
+In a file called response.py, using either validator-collection or validators from PyPI, implement a program that prompts the 
+user for an email address via input and then prints Valid or Invalid, respectively, if the input is a syntatically valid email 
+address. You may not use re. And do not validate whether the email address’s domain name actually exists.
+
+In a file called response.py, using either validator-collection or validators from PyPI, implement a program that prompts the user
+for an email address via input and then prints Valid or Invalid, respectively, if the input is a syntatically valid email address.
+You may not use re. And do not validate whether the email address’s domain name actually exists.
+'''
+#############################################################################################################################
+
